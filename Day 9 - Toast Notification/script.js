123bf62b877057cf872731cfc90d5c5d657f5d13
Rusiru-Jayakody <jayakodyrusiru@gmail.com>
2025-02-03 02:04:41 +0530

    Toast Notification project completed

--- Day 9 - Toast Notification/script.js
+++ Day 9 - Toast Notification/script.js
@@ -4,13 +4,29 @@ const errorBtn = document.getElementById("error");
 const invalidBtn = document.getElementById("invalid");
 let toastBox = document.getElementById("toastBox");
 
-function showToast(){
+let successMsg = '<i class="fa-regular fa-circle-check"></i> Successfully submitted';
+let errorMsg = '<i class="fa-solid fa-circle-xmark"></i> Please fix the error !';
+let invalidMsg = '<i class="fa-solid fa-circle-exclamation"></i> Invalid input, check again';
+
+function showToast(msg){
     let toast = document.createElement('div');
     toast.classList.add('toast');
-    toast.innerHTML = 'success';
+    toast.innerHTML = msg;
     toastBox.appendChild(toast);
+
+    if(msg.includes('error')){
+        toast.classList.add('error');
+    }
+
+    if(msg.includes('Invalid')){
+        toast.classList.add('invalid');
+    }
+
+    setTimeout(() =>{
+        toast.remove();
+    },6000)
 }
 
-successBtn.onclick = showToast;
-errorBtn.onclick = showToast;
-invalidBtn.onclick = showToast;
+successBtn.onclick = () => {showToast(successMsg);};
+errorBtn.onclick = () => {showToast(errorMsg);};
+invalidBtn.onclick = () => {showToast(invalidMsg);};
